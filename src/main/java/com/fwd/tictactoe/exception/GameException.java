badd93426c20ae52a250ff9f62fc4e4e08483fef
octahori <saptariwijaya4@gmail.com>
2025-07-05 10:29:38 +0700

    feat: add new controller, engine, service, etc

---
+++ src/main/java/com/fwd/tictactoe/exception/GameException.java
@@ -0,0 +1,7 @@
+package com.fwd.tictactoe.exception;
+
+public class GameException extends RuntimeException {
+    public GameException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/fwd/tictactoe/exception/InvalidMoveException.java
@@ -0,0 +1,7 @@
+package com.fwd.tictactoe.exception;
+
+public class InvalidMoveException extends GameException {
+    public InvalidMoveException(String message) {
+        super(message);
+    }
+}
